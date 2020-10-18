d6b5f551c01bac5e1a2aa1109c168b844427f263
unknown <sammymenaged@yahoo.com>
2020-10-18 05:12:30 -0400

    added engines to package.json

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/blogDB" , { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("Connected to DB");
+});
 
 const postSchema = {
   title: String,
